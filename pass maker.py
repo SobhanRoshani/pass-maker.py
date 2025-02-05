f1aa3b4fc06255b4ce6db4af0a78b5f70c9d5d2c
Sobhan <rou.sobhan@gmail.com>
2025-02-05 20:55:38 +0330

    pass make

---
+++ pass maker.py
@@ -0,0 +1,8 @@
+Firstname = input("input you're first name: ")
+Lastname = str(input("input you're last name: "))
+age = int(input("input you're age: "))
+y = int(1403)
+k = str(y - age)
+x = '@'
+h = (Lastname+x+k)
+print("it is your own password:", h)
